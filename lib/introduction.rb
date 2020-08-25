3770c3faa4a61114fa6b7eaa6ae94d950994b598
Jeffrey Bolling <j2b2590@gmail.com>
2020-08-25 16:56:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,7 +10,11 @@ end
 
 introduction "Josh"
 
+def introduction_with_language (name, language)
+  "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
+introduction_with_language
 
 
 
